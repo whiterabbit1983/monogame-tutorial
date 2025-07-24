be181b3f07f8c7fc6c922c5450dfca2ab524be8b
Dmitry Paramonov <asmatic075@gmail.com>
2025-07-24 15:24:50 +0300

    feat: Add mouse info

---
+++ MonoGameLibrary/Input/MouseInfo.cs
@@ -0,0 +1,112 @@
+using System.Text.RegularExpressions;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace MonoGameLibrary.Input;
+
+public class MouseInfo
+{
+    public MouseState PreviousState { get; private set; }
+    public MouseState CurrentState { get; private set; }
+    public Point Position
+    {
+        get => CurrentState.Position;
+        set => SetPosition(value.X, value.Y);
+    }
+    public int X
+    {
+        get => CurrentState.X;
+        set => SetPosition(value, CurrentState.Y);
+    }
+
+    public int Y
+    {
+        get => CurrentState.Y;
+        set => SetPosition(CurrentState.Y, value);
+    }
+    public Point PositionDelta => CurrentState.Position - PreviousState.Position;
+    public int XDelta => CurrentState.X - PreviousState.X;
+    public int YDelta => CurrentState.Y - PreviousState.Y;
+    public bool WasMoved => PositionDelta != Point.Zero;
+    public int ScrollWheel => CurrentState.ScrollWheelValue;
+    public int ScrollWheelDelta => CurrentState.ScrollWheelValue - PreviousState.ScrollWheelValue;
+
+    public void SetPosition(int x, int y)
+    {
+        Mouse.SetPosition(x, y);
+        CurrentState = new(
+            x,
+            y,
+            CurrentState.ScrollWheelValue,
+            CurrentState.LeftButton,
+            CurrentState.MiddleButton,
+            CurrentState.RightButton,
+            CurrentState.XButton1,
+            CurrentState.XButton2
+        );
+    }
+
+    public MouseInfo()
+    {
+        PreviousState = new();
+        CurrentState = Mouse.GetState();
+    }
+
+    public void Update()
+    {
+        PreviousState = CurrentState;
+        CurrentState = Mouse.GetState();
+    }
+
+    public bool IsButtonDown(MouseButton button)
+    {
+        return button switch
+        {
+            MouseButton.Left => CurrentState.LeftButton == ButtonState.Pressed,
+            MouseButton.Middle => CurrentState.MiddleButton == ButtonState.Pressed,
+            MouseButton.Right => CurrentState.RightButton == ButtonState.Pressed,
+            MouseButton.XButton1 => CurrentState.XButton1 == ButtonState.Pressed,
+            MouseButton.XButton2 => CurrentState.XButton2 == ButtonState.Pressed,
+            _ => false,
+        };
+    }
+
+    public bool IsButtonUp(MouseButton button)
+    {
+        return button switch
+        {
+            MouseButton.Left => CurrentState.LeftButton == ButtonState.Released,
+            MouseButton.Middle => CurrentState.MiddleButton == ButtonState.Released,
+            MouseButton.Right => CurrentState.RightButton == ButtonState.Released,
+            MouseButton.XButton1 => CurrentState.XButton1 == ButtonState.Released,
+            MouseButton.XButton2 => CurrentState.XButton2 == ButtonState.Released,
+            _ => false,
+        };
+    }
+
+    public bool WasButtonJustPressed(MouseButton button)
+    {
+        return button switch
+        {
+            MouseButton.Left => CurrentState.LeftButton == ButtonState.Pressed && PreviousState.LeftButton == ButtonState.Released,
+            MouseButton.Middle => CurrentState.MiddleButton == ButtonState.Pressed && PreviousState.MiddleButton == ButtonState.Released,
+            MouseButton.Right => CurrentState.RightButton == ButtonState.Pressed && PreviousState.RightButton == ButtonState.Released,
+            MouseButton.XButton1 => CurrentState.XButton1 == ButtonState.Pressed && PreviousState.XButton1 == ButtonState.Released,
+            MouseButton.XButton2 => CurrentState.XButton2 == ButtonState.Pressed && PreviousState.XButton2 == ButtonState.Released,
+            _ => false,
+        };
+    }
+
+    public bool WasButtonJustReleased(MouseButton button)
+    {
+        return button switch
+        {
+            MouseButton.Left => CurrentState.LeftButton == ButtonState.Released && PreviousState.LeftButton == ButtonState.Pressed,
+            MouseButton.Middle => CurrentState.MiddleButton == ButtonState.Released && PreviousState.MiddleButton == ButtonState.Pressed,
+            MouseButton.Right => CurrentState.RightButton == ButtonState.Released && PreviousState.RightButton == ButtonState.Pressed,
+            MouseButton.XButton1 => CurrentState.XButton1 == ButtonState.Released && PreviousState.XButton1 == ButtonState.Pressed,
+            MouseButton.XButton2 => CurrentState.XButton2 == ButtonState.Released && PreviousState.XButton2 == ButtonState.Pressed,
+            _ => false,
+        };
+    }
+}
