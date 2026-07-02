fcb87e60317d6309c18a1306cd552deab293e20b
Dmitry Paramonov <asmatic075@gmail.com>
2026-07-02 16:33:20 +0300

    chore: Rename static vars

--- DungeonSlime/GameController.cs
+++ DungeonSlime/GameController.cs
@@ -7,23 +7,23 @@ namespace DungeonSlime;
 
 public static class GameController
 {
-    private static KeyboardInfo s_keyboard => Core.Input.Keyboard;
-    private static GamePadInfo s_gamePad => Core.Input.GamePads[(int)PlayerIndex.One];
+    private static KeyboardInfo Keyboard => Core.Input.Keyboard;
+    private static GamePadInfo GamePad => Core.Input.GamePads[(int)PlayerIndex.One];
 
     public static bool MoveUp()
     {
-        return s_keyboard.WasKeyJustPressed(Keys.Up) ||
-               s_keyboard.WasKeyJustPressed(Keys.W) ||
-               s_gamePad.WasButtonJustPressed(Buttons.DPadUp) ||
-               s_gamePad.WasButtonJustPressed(Buttons.LeftThumbstickUp);
+        return Keyboard.WasKeyJustPressed(Keys.Up) ||
+               Keyboard.WasKeyJustPressed(Keys.W) ||
+               GamePad.WasButtonJustPressed(Buttons.DPadUp) ||
+               GamePad.WasButtonJustPressed(Buttons.LeftThumbstickUp);
     }
 
     public static bool MoveDown()
     {
-        return s_keyboard.WasKeyJustPressed(Keys.Down) ||
-               s_keyboard.WasKeyJustPressed(Keys.S) ||
-               s_gamePad.WasButtonJustPressed(Buttons.DPadDown) ||
-               s_gamePad.WasButtonJustPressed(Buttons.LeftThumbstickDown);
+        return Keyboard.WasKeyJustPressed(Keys.Down) ||
+               Keyboard.WasKeyJustPressed(Keys.S) ||
+               GamePad.WasButtonJustPressed(Buttons.DPadDown) ||
+               GamePad.WasButtonJustPressed(Buttons.LeftThumbstickDown);
     }
 
     /// <summary>
@@ -31,29 +31,29 @@ public static class GameController
     /// </summary>
     public static bool MoveLeft()
     {
-        return s_keyboard.WasKeyJustPressed(Keys.Left) ||
-               s_keyboard.WasKeyJustPressed(Keys.A) ||
-               s_gamePad.WasButtonJustPressed(Buttons.DPadLeft) ||
-               s_gamePad.WasButtonJustPressed(Buttons.LeftThumbstickLeft);
+        return Keyboard.WasKeyJustPressed(Keys.Left) ||
+               Keyboard.WasKeyJustPressed(Keys.A) ||
+               GamePad.WasButtonJustPressed(Buttons.DPadLeft) ||
+               GamePad.WasButtonJustPressed(Buttons.LeftThumbstickLeft);
     }
 
     public static bool MoveRight()
     {
-        return s_keyboard.WasKeyJustPressed(Keys.Right) ||
-               s_keyboard.WasKeyJustPressed(Keys.D) ||
-               s_gamePad.WasButtonJustPressed(Buttons.DPadRight) ||
-               s_gamePad.WasButtonJustPressed(Buttons.LeftThumbstickRight);
+        return Keyboard.WasKeyJustPressed(Keys.Right) ||
+               Keyboard.WasKeyJustPressed(Keys.D) ||
+               GamePad.WasButtonJustPressed(Buttons.DPadRight) ||
+               GamePad.WasButtonJustPressed(Buttons.LeftThumbstickRight);
     }
 
     public static bool Pause()
     {
-        return s_keyboard.WasKeyJustPressed(Keys.Escape) ||
-               s_gamePad.WasButtonJustPressed(Buttons.Start);
+        return Keyboard.WasKeyJustPressed(Keys.Escape) ||
+               GamePad.WasButtonJustPressed(Buttons.Start);
     }
 
     public static bool Action()
     {
-        return s_keyboard.WasKeyJustPressed(Keys.Enter) ||
-               s_gamePad.WasButtonJustPressed(Buttons.A);
+        return Keyboard.WasKeyJustPressed(Keys.Enter) ||
+               GamePad.WasButtonJustPressed(Buttons.A);
     }
 }
